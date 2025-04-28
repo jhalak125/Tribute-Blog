170ec21c065752f0c64668cfffc3c192a1151021
Jhalak Verma <jhalakmverma.15@gmail.com>
2025-04-28 14:44:58 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
-// HomePage Code Placeholder
-import React from 'react';
+import ArticleCard from '../components/ArticleCard';
+import articles from '../data/articles.json';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page!</h1>
+    <div className="container mx-auto p-6">
+      <h1 className="text-4xl font-bold mb-6">Welcome to the Tribute Blog</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {articles.map((article) => (
+          <ArticleCard key={article.id} article={article} />
+        ))}
+      </div>
     </div>
   );
 };
