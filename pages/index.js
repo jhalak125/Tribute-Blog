1569c4a0e875f7eab4e177c868b928ee124205c6
Jhalak Verma <jhalakmverma.15@gmail.com>
2025-04-28 14:29:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
 // HomePage Code Placeholder
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
